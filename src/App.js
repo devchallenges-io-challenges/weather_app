bd5cd032bed4191a130bbe97a365d3ac85808e24
MJ Lynch <mjlynch35@yahoo.com>
2025-01-22 19:17:14 -0600

    Adding styling and backend data to those areas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
+import react, { useState, useEffect, use } from 'react';
 import './App.css';
 import Nav from './Components/Nav';
+import MainCard from './Components/MainCard';
 
 function App() {
+  const [weatherData, setWeatherData] = useState([]);
+  const [currentTemp, setCurrentTemp] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const city = "Rockford";
+
+  const apiKey = "d364742c2d8b7f2f0c576a34aeaca478";
+  const link = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  const link2 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch(link);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status} `);
+        }
+        const data = await response.json();
+        console.log("Data: ", data);
+        setWeatherData(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWeather();
+  }, []); // Empty dependency array ensures it runs only once on mount
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch(link2);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status} `);
+        }
+        const data = await response.json();
+        console.log("Data: ", data);
+        setCurrentTemp(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWeather();
+  }, []);
+
+  if (loading) return <p>Loading weather data...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+
   return (
     <div className="App">
       <nav>
@@ -9,10 +66,20 @@ function App() {
       </nav>
       <section className='upper'>
         <div className='upper-left'>
-
+          <MainCard currentTemp={currentTemp} />
         </div>
 
         <div className='upper-right'>
+
+        </div>
+      </section>
+
+      <section className='lower'>
+        <div className='lower-left'>
+
+        </div>
+
+        <div className='lower-right'>
 
         </div>
       </section>
---
+++ src/Components/MainCard.js
@@ -0,0 +1,14 @@
+import react, { useState, useEffect } from 'react';
+import "../CSS/MainCard.css";
+
+export default function MainCard({ weatherTemp }) {
+    return (
+        <>
+            <div className='main-card'>
+                <div className='main-card-container'>
+
+                </div>
+            </div>
+        </>
+    )
+}
