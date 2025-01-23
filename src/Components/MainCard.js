767cd55cdff0216f6ae1d3b4236fcc68bbc08c1c
MJ Lynch <mjlynch35@yahoo.com>
2025-01-22 19:40:47 -0600

    Working on main card

--- src/Components/MainCard.js
+++ src/Components/MainCard.js
@@ -1,12 +1,34 @@
-import react, { useState, useEffect } from 'react';
+import react, { useState, useEffect, use } from 'react';
 import "../CSS/MainCard.css";
 
-export default function MainCard({ weatherTemp }) {
+export default function MainCard({ currentTemp }) {
+    const [currentTime, setCurrentTime] = useState('');
+
+    useEffect(() => {
+        const updateTime = () => {
+            const now = new Date();
+            const formattedTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
+            setCurrentTime(formattedTime);
+        };
+
+        updateTime(); // Set initial time
+        const interval = setInterval(updateTime, 60000); // Update every minute
+
+        return () => clearInterval(interval); // Cleanup on unmount
+    }, []);
+
+
     return (
         <>
             <div className='main-card'>
                 <div className='main-card-container'>
-
+                    <div className='top-row'>
+                        <div className='current-temp'>{Math.round(currentTemp.main.temp)}°</div>
+                        <div className='city-time'>
+                            <div className='city'>{currentTemp.name}</div>
+                            <div className='time'>{currentTime}</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
