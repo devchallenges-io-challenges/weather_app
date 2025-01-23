e755cb5a270aeccf38b015cd1c13ca3f905dc854
MJ Lynch <mjlynch35@yahoo.com>
2025-01-23 16:21:49 -0600

    Added otherCities.js

---
+++ src/Components/OtherCities.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "../CSS/OtherCities.css";
+
+export default function OtherCities({ weatherData, getWeatherIcon }) {
+    return (
+        <>
+
+        </>
+    )
+}
